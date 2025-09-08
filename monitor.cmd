74d72ea8ffb52cac354c22aaa0b610e4a7cd83d9
Paul Fornage <paul@fornage.net>
2025-09-08 07:13:08 -0700

    add standalone monitor

---
+++ monitor.cmd
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+echo Starting ClearCore flash and serial monitor...
+echo.
+
+
+echo Starting serial monitor...
+echo Press Ctrl+C to exit serial monitor when done.
+echo.
+
+
+REM Start the serial monitor
+"data\SimplySerial_0.9.0_standalone\ss.exe"
+
+echo.
+echo Script completed.
