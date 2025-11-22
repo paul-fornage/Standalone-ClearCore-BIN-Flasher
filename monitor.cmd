231510aefb68b7acb372bc8cd97b413fee70c6ec
Paul Fornage <paul@fornage.net>
2025-11-22 08:36:21 -0800

    Clean up erroneous echo

--- monitor.cmd
+++ monitor.cmd
@@ -1,14 +1,10 @@
 @echo off
 setlocal
 
-echo Starting ClearCore flash and serial monitor...
-echo.
-
 
 echo Starting serial monitor...
 echo Press Ctrl+C to exit serial monitor when done.
 echo.
-
 
 REM Start the serial monitor
 "data\SimplySerial_0.9.0_standalone\ss.exe"
